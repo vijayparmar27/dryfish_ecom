d31fd92745e176ab866ed4942d348c25e8754808
vijay parmar <vijayparmar0027@gmail.com>
2025-11-18 09:58:03 +0530

    add product list

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { ProductsView } from "@/sections/product/view";
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <ProductsView />
+    </div>
+  );
+};
+
+export default Products;
