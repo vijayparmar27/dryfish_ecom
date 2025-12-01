42e2f2fa98ed4c921973b278c01fd7c9ce502dab
Vijay Parmar <vijayparmar0027@gmail.com>
2025-12-01 14:22:17 +0530

    feat: implement blog post detail page with static generation

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,5 +1,13 @@
 import PostDetailsView from "@/sections/blogs/view/post-details-view";
+import { _posts } from "@/_mock";
 import React from "react";
+
+// Generate static params for all blog posts
+export async function generateStaticParams() {
+  return _posts.map((post) => ({
+    id: post.id,
+  }));
+}
 
 const Blogs = () => {
   console.log("Blogs");
